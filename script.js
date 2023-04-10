a773fe17815bc216e719aafcfaa8bf187ed77a67
abdul rehman e <abdulrkhan570@gmail.com>
2023-04-11 02:13:58 +0530

    Animation added

--- script.js
+++ script.js
@@ -1,58 +1,112 @@
-let previous_swap = {};
-// to check if the drop is valid or not 
-let isValid;
+let last_swap;
+let oldCell;
+let swaps = [];
+
+
 // drag function called when dragstarts
 function drag(e) {
-// storing the dragged element id, used in dropping that element
-    e.dataTransfer.setData("text", e.target.id);
-    e.target.className += ' hold'
-    setTimeout(()=> {e.target.className = 'invisible'},0); // using setTimeout so that it becomes invisibile after we grab it 
-    isValid = false;
+  oldCell = e.target;
+  e.target.className += " hold";
 }
+
 // drop function called when element drops;
 function drop(e) {
-// prevent the default action of the event 
-    e.preventDefault();
-// storing newelement 
-    const newCell = e.target;  
-    const newClone = e.target.cloneNode(true);
-// retrieving old element using id ;
-    const oldId = e.dataTransfer.getData("text");
-    const oldCell = document.getElementById(oldId);
-// drop is successfull making element visible again
-    oldCell.className = 'box'
+  // prevent the default action of the event
+  e.preventDefault();
+  document.getElementById('flash').innerHTML = "";
 
-// storing prev elements id
-    previous_swap.old_id = oldId;
-    previous_swap.new_id = e.target.id;
+  // storing newelement
+  const newCell = e.target;
 
+  last_swap = [oldCell, newCell];
 
-    // making clone of old element
-    const oldClone = oldCell.cloneNode(true);
-// replacing old with new
-    oldCell.parentNode.replaceChild(newClone, oldCell);
-// replacing new with old
-    newCell.parentNode.replaceChild(oldClone,newCell);
-    isValid = true;// successfull drop
-  }
-  function dragend(e) {
-    if(isValid === false){// if not successfull drop then make the element visible 
-       e.target.className = "box";
+  // storing the swaps
+  swaps.push(last_swap);
+
+  // copying info of old Cell
+  const tempContent = oldCell.innerHTML;
+  const tempColor = oldCell.style.backgroundColor;
+
+  // exchanging new cell with oldCell
+  oldCell.innerHTML = newCell.innerHTML;
+  oldCell.style.backgroundColor = newCell.style.backgroundColor;
+
+  // replacing new cell with oldCell
+  newCell.innerHTML = tempContent;
+  newCell.style.backgroundColor = tempColor;
+
+  // Gettting the coordinate of oldCell and newCell
+  let oldCell_coordinate = oldCell.getBoundingClientRect();
+  let newCell_coordinate = newCell.getBoundingClientRect();
+
+  //distance btw oldCell and newCell
+  let change_in_X = newCell_coordinate.left - oldCell_coordinate.left;
+  let change_in_Y = newCell_coordinate.top - oldCell_coordinate.top;
+
+  // animation from new cell to old cell;
+  let animation = oldCell.animate(
+    [
+      { transform: `translate(${change_in_X}px, ${change_in_Y}px)` },
+      { transform: "none" },
+    ],
+    {
+      duration: 900,
+      easing: "ease-in",
     }
-  }
+  );
+
+  animation.onfinish = () => {
+    oldCell.className = "box";
+  };
+}
+
+function dragend(e) {
+  oldCell.className = "box";
+}
 function allowDrop(e) {
   e.preventDefault();
 }
 // function for undoing last operation
-function undo(){
-// retrieving prev elements using id ;
-    const old_Id = previous_swap.old_id;
-    const oldCell = document.getElementById(old_Id);
-    const new_Id = previous_swap.new_id;
-    const newCell = document.getElementById(new_Id);
-    const newClone = newCell.cloneNode(true);
-    const oldClone = oldCell.cloneNode(true);
-// switching their position again on undo
-    oldCell.parentNode.replaceChild(newClone, oldCell);
-    newCell.parentNode.replaceChild(oldClone,newCell);
+function undo() {
+  // if their are no swaps to undo
+  if(swaps.length === 0){
+    document.getElementById('flash').innerHTML = "no moves left to undo ";
+    return
+  }
+  // getting the cells to be undo
+  const newCell = swaps[swaps.length - 1][0];
+  const oldCell = swaps[swaps.length - 1][1];
+  swaps.pop(); // removing the swaps from the array
+  // adding the opacity class
+  oldCell.className += " hold";
+   // storing info of old Cell
+  const tempContent = oldCell.innerHTML;
+  const tempColor = oldCell.style.backgroundColor;
+
+  //updating oldCell
+  oldCell.innerHTML = newCell.innerHTML;
+  oldCell.style.backgroundColor = newCell.style.backgroundColor;
+
+  //updating newCell
+  newCell.innerHTML = tempContent;
+  newCell.style.backgroundColor = tempColor;
+
+  const oldCell_coordinate = oldCell.getBoundingClientRect();
+  const newCell_coordinate = newCell.getBoundingClientRect();
+  const change_in_X = newCell_coordinate.left - oldCell_coordinate.left;
+  const change_in_Y = newCell_coordinate.top - oldCell_coordinate.top;
+  const animation = oldCell.animate(
+    [
+      { transform: `translate(${change_in_X}px, ${change_in_Y}px)` },
+      { transform: "none" },
+    ],
+    {
+      duration: 900,
+      easing: "ease-in",
+    }
+  );
+
+  animation.onfinish = () => {
+    oldCell.className = "box";
+  };
 }
